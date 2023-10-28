c05170da5680b6a3ee57a90569752c49bd6be826
Partha1X <nimbus2kilo@gmail.com>
2023-10-28 10:32:26 +0530

    28-10-2023

---
+++ 2023/October/28-10-2023.cpp
@@ -0,0 +1,54 @@
+/*
+
+Author : Partha Biswas
+Date : 28/10/2023
+Problem : Bleak Numbers
+Problem Link : https://practice.geeksforgeeks.org/problems/bleak-numbers1552/1
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution
+    {
+public:
+    int  countSetBit(int n){
+    int ans=0;
+    while(n){
+        if(n&1) ans++;
+        n/=2;
+}
+    return ans;
+}
+    int is_bleak(int n)
+	{
+	    // Code here.
+	     int j=1;
+        while((1<<j)< n) j++;
+        
+        for(int i=n-j;i<n;i++){
+          if(countSetBit(i)+i==n) return 0;
+        }  
+      return 1;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main(){
+    int T;
+    cin >> T;
+    while(T--)
+    {
+    	int n; 
+    	cin >> n;
+    	Solution ob;
+    	int ans = ob.is_bleak(n);
+    	cout << ans << "\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
