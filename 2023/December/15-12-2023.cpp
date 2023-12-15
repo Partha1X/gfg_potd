f8521e339a1a372ab895cd6f7cca084c48bdbf9c
Partha1X <nimbus2kilo@gmail.com>
2023-12-15 23:53:16 +0530

    Added on 15-12-2023

---
+++ 2023/December/15-12-2023.cpp
@@ -0,0 +1,46 @@
+/*
+
+Author : Partha Biswas
+Date : 15/12/2023
+Problem : Reach the Nth point
+Problem Link : https://www.geeksforgeeks.org/problems/reach-the-nth-point5433/1
+*/
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+		int nthPoint(int n){
+		    // Code here
+		    long long a, b;
+		    a = b = 1;
+		    
+		    const long long mod = 1e9 + 7;
+		    
+		    for(int i = 1; i < n; i++){
+		        long long c = (a + b) % mod;
+		        a = b;
+		        b = c;
+		    }
+		    
+		    return b;
+		}
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n;
+		cin >> n;
+		Solution ob;
+		int ans  = ob.nthPoint(n);
+		cout << ans <<"\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
